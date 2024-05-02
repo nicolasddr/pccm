9b7b3907c88fa4f984816946c11f85a1b7c277f3
Nicolas Rodrigues <105825829+nicolasddr@users.noreply.github.com>
2024-05-02 16:59:05 -0400

    Implementação da Lista de Adjacências

--- pccm.c
+++ pccm.c
@@ -1,6 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct Node{
+    int vertice;
+    int peso;
+    struct Node *prox;
+} Node;
+
+typedef struct {
+    int n_vertices;
+    int n_arcos;
+    Node **adjacencias;
+} Grafo;
+
+void inicializaGrafo(Grafo *grafo, int n_vertices){
+    grafo->n_vertices = n_vertices;
+    grafo->n_arcos = 0;
+    grafo->adjacencias = (Node**)malloc(n_vertices * sizeof(Node*));
+    for(int i=0; i<n_vertices; i++){
+        grafo->adjacencias[i] = NULL;
+    }
+}
+
+void imprimirGrafo(Grafo *grafo){
+    printf("\nImprimindo grafo com %d vértices e %d arcos\n", grafo->n_vertices, grafo->n_arcos);
+    Node* atual;
+    for(int i=0; i<grafo->n_vertices; i++){
+        printf("[%d]", i);
+        atual = grafo->adjacencias[i];
+        while(atual){
+            printf(" ->%d (%d)", atual->vertice, atual->peso);
+            atual = atual->prox;
+        }
+        printf("\n");
+    }
+}
+
+void insereAresta(Grafo *grafo, int v1, int v2, int peso){
+    Node *novo, *ant = NULL;
+    Node *atual = grafo->adjacencias[v1];
+    while(atual && atual->vertice<v2){
+        ant = atual;
+        atual = atual->prox;
+    }
+
+    novo = (Node*)malloc(sizeof(Node));
+    novo->vertice = v2;
+    novo->peso = peso;
+    novo->prox = atual;
+
+    if(ant) ant->prox = novo;
+    else grafo->adjacencias[v1] = novo;
+
+    grafo->n_arcos++;
+
+}
+
 int main(int argc, char *argv[]){
 
     char *arquivo = argv[1];
@@ -20,23 +75,33 @@ int main(int argc, char *argv[]){
     //Cria um vetor para armazenar uma linha
     char linha[256];
 
+
     //Armazena a entrada de uma linha
     fgets(linha, 256, entrada);
 
     //Lê o a primeira linha e armazena os valores do número de vértices e arcos
     sscanf(linha, "%c %d %d", &id, &n_vertices, &n_arcos);
 
+    
+
+    Grafo *grafo = (Grafo*)malloc(sizeof(Grafo)); 
+    inicializaGrafo(grafo, n_vertices);
+
     //Lê cada linha e armazena os valores dos vértices e arcos nas respectivas variáveis
     while(id != 'T'){
         fgets(linha, 256, entrada); 
         sscanf(linha, "%c %d %d %d", &id, &a, &b, &c);
-        printf("%c ", id);
-        printf("%d ", a);
-        printf("%d ", b);
-        printf("%d\n", c);
+
+        if(id == 'N'){
+            printf("vertice");
+        } else if(id == 'E'){
+            insereAresta(grafo, a, b, c);
+        }
+
+        
     }
 
-    
+    imprimirGrafo(grafo);
 
     return 0;
 }
